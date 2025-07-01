dbbe417dbb89a07fa182133e7efb8832df9a76f2
T-SGW <sgwtthkiviiv@gmail.com>
2025-07-01 21:20:37 +0900

    setup

---
+++ src/test/java/com/study/AppTest.java
@@ -0,0 +1,12 @@
+package com.study;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
